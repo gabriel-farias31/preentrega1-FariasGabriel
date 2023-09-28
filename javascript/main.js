c56fe56ec2a430bc29a31f2c21b7c58b15cd7231
Gabriel Farias <gabrielfariasok31@gmail.com>
2023-09-28 08:11:50 -0300

    Preentrega2fariasgabriel

--- javascript/main.js
+++ javascript/main.js
@@ -50,3 +50,28 @@ switch (hora) {
 }
 
 alert("su turno es el dia" + " "+ turnos+" "+ "a las" + " " + hora+ "horas")
+
+
+class Clienta {
+    constructor(nombres,edad,color){
+    this.nombres = nombres;
+    this.edad = edad;
+    this.color = color;
+}
+}
+
+const clienta1 = new Clienta("Andrea", "65", "9.4");
+const clienta2 = new Clienta("Ivana", "26", "6.3");
+const clienta3 = new Clienta("Sofia", "40", "7");
+
+const clientas =[clienta1, clienta2, clienta3];
+
+clientas.push ("clienta4");
+console.log (clientas)
+
+clientas.forEach(clientas => {
+    console.log("nombres:", clientas.nombres);
+    console.log("edad:", clientas.edad);
+    console.log("color:", clientas.color);
+    console.log("----------");
+  });
